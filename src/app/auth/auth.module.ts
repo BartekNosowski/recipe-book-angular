bb92b3fcd047485f0397a078878b80dae9659c91
Bartosz Nosowski <barteknosowski@gmail.com>
2022-01-19 16:49:50 +0100

    page background changed; added collapse in header on mobile resolution; added validation to login/signup panel

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,15 +4,17 @@ import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {SharedModule} from '../shared/shared.module';
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
   declarations: [AuthComponent],
-  imports: [
-    CommonModule,
-    FormsModule,
-    RouterModule.forChild([{path: '', component: AuthComponent}]),
-    SharedModule
-  ]
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild([{path: '', component: AuthComponent}]),
+        SharedModule,
+        TranslateModule
+    ]
 })
 export class AuthModule {
 }
